c08bcba72636dab4df2e03df14996bcd21ff5a10
Hakim Horairy <horairyhakim@gmail.com>
2024-01-09 14:10:36 +0100

    adding extraction function

--- hidden_watermark.py
+++ hidden_watermark.py
@@ -27,5 +27,34 @@ def lsb1_stegano(image_path, message):
 	cv2.imwrite("../watermarked_image.png", image_array)
 
 
+def lsb1_extract_message(watermarked_image_path):
+	watermarked_image_array = cv2.imread(watermarked_image_path)
+	binary_array_message = watermarked_image_array % 2
+
+	nb_rows, nb_cols, nb_canals = binary_array_message.shape
+
+	binary_message_list = []
+
+	for index_row in range(nb_rows):
+		for index_col in range(nb_cols):
+			for index_canal in range(nb_canals):
+
+				binary_message_list.append(str(binary_array_message[index_row, index_col, index_canal]))
+
+
+	for index_binary_char in range(0, nb_rows*nb_cols*nb_canals, 8) :
+		if binary_message_list[index_binary_char: index_binary_char+8] == ["0"] * 8 :
+			binary_message_list = binary_message_list[ : index_binary_char]
+			break
+	
+
+	binary_message = "".join(binary_message_list)
+
+	message = "".join([chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8)])
+
+	print(message)
+
+
 if __name__ == "__main__":
 	lsb1_stegano("../aladin.jpg", "Coucou les loulous")
+	lsb1_extract_message("../watermarked_image.png")
